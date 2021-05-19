507a0a182761d4c5836ad5ebd6f25fc18f0df122
unknown <silvercrow108@gmail.com>
2021-05-19 08:43:43 +0800

    fixed form css

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -3,36 +3,39 @@ export default function Footer() {
     <footer className="footer-main">
       <ul className="bottom">
         <li className="bottom__item">
-          <a className="bottom__link" href="">
-            Github
-          </a>
-        </li>
+          <a className="bottom__link" href="https://github.com/krisyana/">
+            Github{' '}
+          </a>{' '}
+        </li>{' '}
+        <li className="bottom__item">
+          <a
+            className="bottom__link"
+            href="https://github.com/krisyana/pet-champ"
+          >
+            Repo{' '}
+          </a>{' '}
+        </li>{' '}
+        <li className="bottom__item">
+          <a className="bottom__link" href="kdkrisnadwipayana@gmail.com">
+            Email{' '}
+          </a>{' '}
+        </li>{' '}
         <li className="bottom__item">
           <a className="bottom__link" href="">
-            Main
-          </a>
-        </li>
+            Website{' '}
+          </a>{' '}
+        </li>{' '}
+        <li className="bottom__item">
+          <a className="bottom__link" href="wa.link/57xepp">
+            WA{' '}
+          </a>{' '}
+        </li>{' '}
         <li className="bottom__item">
           <a className="bottom__link" href="">
-            Email
-          </a>
-        </li>
-        <li className="bottom__item">
-          <a className="bottom__link" href="">
-            Website
-          </a>
-        </li>
-        <li className="bottom__item">
-          <a className="bottom__link" href="">
-            WA
-          </a>
-        </li>
-        <li className="bottom__item">
-          <a className="bottom__link" href="">
-            Line
-          </a>
-        </li>
-        <p className="copyright">&copy; Copyright 2021 by Krisna Dwipayana</p>
+            Line{' '}
+          </a>{' '}
+        </li>{' '}
+        <p className="copyright">&copy; Copyright 2021 by Krisna Dwipayana </p>
       </ul>
     </footer>
   );
